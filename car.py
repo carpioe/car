257e16f173f08868d68380b22c4ba8645cebfc9c
eddie <carpioe@hartwick.edu>
2019-11-12 08:52:04 -0500

    updated

--- car.py
+++ car.py
@@ -1,3 +1,14 @@
 #eddie carpio
 #11/12/19
 #attributes and descriptions
+class car:
+    def __init__(self,make,model,color,year,price,mileage):
+        self.make = make
+        self.model = model
+        self.color = color
+        self.year = year
+        self.price = price
+        self.mileage = mileage
+    def sit(self):
+        print(self.make + "is a very new nissan altima")
+
