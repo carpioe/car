7a6c1ae98b55f44cf4ffbf961a212d4fc9cddc15
carpioe <56162609+carpioe@users.noreply.github.com>
2019-11-14 08:41:03 -0500

    Update car.py

--- car.py
+++ car.py
@@ -1,14 +1,23 @@
-#eddie carpio
-#11/12/19
-#attributes and descriptions
-class car:
-    def __init__(self,make,model,color,year,price,mileage):
-        self.make = make
+class car():
+
+    def __init__(self,name,model,color,year,price,mileage,new_color,new_mileage):
+        self.name = name
         self.model = model
         self.color = color
         self.year = year
         self.price = price
         self.mileage = mileage
-    def sit(self):
-        print(self.make + "is a very new nissan altima")
+        self.new_color = new_color
+        self.new_mileage = new_mileage
+    def description(self):
+        print(f"""My car is a {self.name},model{self.model}color {self.color}.
+                {self.price} and today's milerage is{self.mileage} """)
+
+    def change_color(self):
+        self.new_color = self.new_color
+        print((f'I am thinking of thange the color of my car to {self.color}'))
+
+    def update_odometer(self):
+        self.mileage = self.mileage + self.new_mileage
+        print(f"my new mileage is {self.mileage}")
 
